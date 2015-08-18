98b7378df14d1912c14f5002e89530f0238e55c6
Hosam Shahin <hosamlshahin@gmail.com>
2015-08-18 15:59:27 -0400

    KA Summ programming questions are working, rebuild NewKA and check for recTracingSumm1

--- utils/codemirror.js
+++ utils/codemirror.js
@@ -1,15 +1,26 @@
 require.config({
-    packages: [{
-        name: "codemirror",
-        location: "/lib/CodeMirror-5.5.0/",
-        main: "lib/codemirror"
-    }]
+	packages: [{
+		name: "codemirror",
+		location: "/lib/CodeMirror-5.5.0/",
+		main: "lib/codemirror"
+	}]
 });
 
 require(["codemirror",
-    "codemirror/mode/clike/clike"
-], function(CodeMirror) {
-    $.extend(KhanUtil, {
-        CodeMirror: CodeMirror
-    });
+	"codemirror/mode/clike/clike"
+], function (CodeMirror) {
+	window.CodeMirror = CodeMirror;
+	// $.extend(KhanUtil, {
+	// 	CodeMirror: CodeMirror
+	// });
+	// window.editor = CodeMirror.fromTextArea(code, {
+	// 	lineNumbers: true,
+	// 	mode: "text/x-java"
+	// });
+	// console.log("in CodeMIrror function");
+	// alert("in CodeMIrror function");
+	// var editor = CodeMirror.fromTextArea(document.getElementById("code"), {
+	// 	lineNumbers: true,
+	// 	mode: "text/x-java",
+	// });
 });
